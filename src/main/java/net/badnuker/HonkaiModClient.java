3d579e2ad06801c8c5b8f2db69c6295327152acb
Badnuker <badnuker@126.com>
2023-07-09 18:18:15 +0800

    Initial commit

---
+++ src/main/java/net/badnuker/HonkaiModClient.java
@@ -0,0 +1,11 @@
+package net.badnuker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HonkaiModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
